ea69d65c2384602194fdfa8c47cfc0bf9539700d
zhengkai <272991962@qq.com>
2020-04-14 22:51:36 +0800

    login ui

--- app/src/main/java/com/example/a27299/cats/MainActivity.kt
+++ app/src/main/java/com/example/a27299/cats/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.a27299.cats
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.a27299.cats.home.HomeActivity
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor= Color.WHITE
+        }
         val sp = applicationContext.getSharedPreferences("cats", Context.MODE_PRIVATE)
         val login = sp.getBoolean("login",false)
         if(login){
@@ -19,5 +24,6 @@ class MainActivity : AppCompatActivity() {
         }else{
             startActivity(Intent(this,LoginActivity::class.java))
         }
+        finish()
     }
 }
--- app/src/main/java/com/example/a27299/cats/login/LoginActivity.kt
+++ app/src/main/java/com/example/a27299/cats/login/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.example.a27299.cats.login
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.a27299.cats.R
@@ -7,6 +9,9 @@ import com.example.a27299.cats.R
 class LoginActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor= Color.WHITE
+        }
         setContentView(R.layout.activity_login)
     }
 }
