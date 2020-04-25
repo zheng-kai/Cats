7abb93c91441c573e6f40d61fc4ae38d8e62e3fc
zhengkai <272991962@qq.com>
2020-04-25 10:00:54 +0800

    登录 注册

--- app/src/main/java/com/example/a27299/cats/login/LoginActivity.kt
+++ app/src/main/java/com/example/a27299/cats/login/LoginActivity.kt
@@ -1,10 +1,9 @@
 package com.example.a27299.cats.login
 
-import android.graphics.Color
-import android.os.Build
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.widget.Toast
 import com.example.a27299.cats.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -12,46 +11,14 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        iv_login.setOnClickListener {
+        iv_login_back.setOnClickListener {
             onBackPressed()
         }
-        initUI()
-        setClickListeners()
-
-    }
-
-    private fun initUI() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            tv_login.setTextColor(resources.getColor(R.color.colorPrimary, null))
+        tv_login_to_register.setOnClickListener{
+            startActivity(Intent(this,RegisterActivity::class.java))
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            tv_login.background = resources.getDrawable(R.drawable.btn_selected_background, null)
-        }
-    }
-
-    private fun setClickListeners() {
-        tv_login.setOnClickListener {
-            til_login_confirm_pwd.visibility = View.INVISIBLE
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                tv_login.setTextColor(resources.getColor(R.color.colorPrimary, null))
-                tv_register.setTextColor(Color.BLACK)
-                tv_register.setBackgroundColor(resources.getColor(R.color.background, null))
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                it.background = resources.getDrawable(R.drawable.btn_selected_background, null)
-            }
-        }
-        tv_register.setOnClickListener {
-            til_login_confirm_pwd.visibility = View.VISIBLE
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                tv_login.setTextColor(Color.BLACK)
-                tv_register.setTextColor(resources.getColor(R.color.colorPrimary, null))
-                tv_login.setBackgroundColor(resources.getColor(R.color.background, null))
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                it.background = resources.getDrawable(R.drawable.btn_selected_background, null)
-            }
+        btn_login.setOnClickListener {
+            Toast.makeText(this,"登录",Toast.LENGTH_LONG).show()
         }
     }
 }
---
+++ app/src/main/java/com/example/a27299/cats/login/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.a27299.cats.login
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.a27299.cats.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        iv_register_back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
