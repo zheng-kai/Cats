f46c3293251fe257a1a5697a54da3d67c176bab7
zhengkai <272991962@qq.com>
2020-04-25 10:47:25 +0800

    home navigation

--- app/src/main/java/com/example/a27299/cats/home/HomeActivity.kt
+++ app/src/main/java/com/example/a27299/cats/home/HomeActivity.kt
@@ -2,12 +2,16 @@ package com.example.a27299.cats.home
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import com.example.a27299.cats.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity (){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        iv_home_menu.setOnClickListener {
+            drawer_home.openDrawer(Gravity.START)
+        }
     }
 }
