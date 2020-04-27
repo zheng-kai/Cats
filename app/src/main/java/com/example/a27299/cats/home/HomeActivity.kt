061f39c49f6156e10f751767ed5ed043007ecb30
zhengkai <272991962@qq.com>
2020-04-27 18:55:41 +0800

    save 右滑筛选 init

--- app/src/main/java/com/example/a27299/cats/home/HomeActivity.kt
+++ app/src/main/java/com/example/a27299/cats/home/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.example.a27299.cats.home
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.View
 import com.example.a27299.cats.R
 import com.example.a27299.cats.home.fragment.HomeFragment
 import com.example.a27299.cats.home.fragment.HomeFragmentPagerAdapter
@@ -16,9 +18,34 @@ class HomeActivity : AppCompatActivity() {
         iv_home_menu.setOnClickListener {
             drawer_home.openDrawer(Gravity.START)
         }
+        iv_home_filter.setOnClickListener {
+            drawer_home.openDrawer(Gravity.RIGHT)
+        }
         vp_home.adapter = HomeFragmentPagerAdapter(supportFragmentManager,
                 listOf(HomeFragment.newInstance(HomeFragment.PICS_TITLE), HomeFragment.newInstance(HomeFragment.SPECIES_TITLE)))
         tab_home.setupWithViewPager(vp_home)
         Module.init()
+        tab_home.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    when (it.text) {
+                        HomeFragment.PICS_TITLE -> {
+                            iv_home_filter.visibility = View.VISIBLE
+                        }
+                        HomeFragment.SPECIES_TITLE -> {
+                            iv_home_filter.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+
+            }
+
+        })
     }
 }
