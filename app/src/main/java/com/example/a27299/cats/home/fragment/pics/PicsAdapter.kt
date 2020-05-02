f6ac98c7ebb9ca7a0ad1aaaa8403c3f2d67cb5c4
zhengkai <272991962@qq.com>
2020-05-02 12:39:15 +0800

    点击看图

--- app/src/main/java/com/example/a27299/cats/home/fragment/pics/PicsAdapter.kt
+++ app/src/main/java/com/example/a27299/cats/home/fragment/pics/PicsAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.a27299.cats.home.fragment.pics
 
+import android.app.AlertDialog
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.StaggeredGridLayoutManager
@@ -10,18 +11,20 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.example.a27299.cats.R
 import com.example.a27299.cats.module.Module
+import kotlinx.android.synthetic.main.dialog_item_home_pic.view.*
 import kotlinx.android.synthetic.main.item_home_pic.view.*
 
 class PicsViewHolder(itemView: View, val ivPic: ImageView) : RecyclerView.ViewHolder(itemView)
 class PicsAdapter(val context: Context?) : RecyclerView.Adapter<PicsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PicsViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_home_pic, parent,false)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_home_pic, parent, false)
         return PicsViewHolder(view, view.iv_item_pic)
     }
 
     override fun getItemCount(): Int {
         return Module.mutableLiveData.value?.size ?: 0
     }
+
     override fun onBindViewHolder(holder: PicsViewHolder, position: Int) {
 
         //固定item大小 防止瀑布流重新计算导致item位置变化
@@ -32,21 +35,37 @@ class PicsAdapter(val context: Context?) : RecyclerView.Adapter<PicsViewHolder>(
             width = screenWidth / 2f
             height = width / this.width.toFloat() * this.height + 0.5f
         }
-        val lp =  StaggeredGridLayoutManager.LayoutParams(width.toInt(),height.toInt())
+        val lp = StaggeredGridLayoutManager.LayoutParams(width.toInt(), height.toInt())
         holder.itemView.layoutParams = lp
-
-        holder.itemView.setOnClickListener {
-
-        }
 
         Module.mutableLiveData.value?.let {
             context?.apply {
+                val url = it[position].url
                 Glide.with(this)
-                        .load(it[position].url)
+                        .load(url)
                         .placeholder(R.drawable.loading)
                         .into(holder.ivPic)
+
+                val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_item_home_pic, null)
+                val d =AlertDialog.Builder(context)
+                        .setView(dialogView)
+                        .create()
+                d.window?.setBackgroundDrawableResource(android.R.color.transparent)
+                holder.itemView.setOnClickListener {
+                    Glide.with(this).load(url)
+                            .placeholder(R.drawable.loading)
+                            .into(dialogView.iv_dialog_img)
+
+
+                    dialogView.iv_dialog_img.setOnClickListener {
+                        d.dismiss()
+                    }
+                    d.show()
+                }
             }
+
         }
+
 
     }
 
--- app/src/main/java/com/example/a27299/cats/module/Authorization.kt
+++ app/src/main/java/com/example/a27299/cats/module/Authorization.kt
@@ -4,7 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
-internal inline val Request.authorized
+inline val Request.authorized: Request
     get() = if (header("x-api-key") == null)
         newBuilder().addHeader("x-api-key", "bb367e23-308c-4384-98fd-e384f6464ff6").build()
     else this
