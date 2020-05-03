807d9497ebaf6990d7e2c05384718cc2c856d82f
zhengkai <272991962@qq.com>
2020-05-03 22:08:38 +0800

    detail

--- app/src/main/java/com/example/a27299/cats/home/fragment/species/DetailAdapter.kt
+++ app/src/main/java/com/example/a27299/cats/home/fragment/species/DetailAdapter.kt
@@ -1,35 +1,48 @@
 package com.example.a27299.cats.home.fragment.species
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.HeaderViewListAdapter
 import android.widget.TextView
+import com.example.a27299.cats.R
+import kotlinx.android.synthetic.main.item_detail_header.view.*
+import kotlinx.android.synthetic.main.item_detail_level.view.*
 
 open class DetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 class DetailHeader(itemView: View, val description: TextView) : DetailViewHolder(itemView)
 class DetailLevel(itemView: View, val type: TextView, val level: TextView) : DetailViewHolder(itemView)
-class DetailAdapter : RecyclerView.Adapter<DetailViewHolder>() {
+class DetailAdapter(private val data: ArrayList<Pair<String, Int>>, private val description: String) : RecyclerView.Adapter<DetailViewHolder>() {
     private val HEADER = 0
     private val LEVEL = 1
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
-        when (viewType) {
+        return when (viewType) {
             HEADER -> {
-
+                val view = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_detail_header, parent, false)
+                DetailHeader(view, view.tv_detail_description)
             }
             else -> {
-
+                val view = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_detail_level, parent, false)
+                DetailLevel(view, view.tv_detail_level_name, view.tv_detail_level)
             }
 
         }
     }
 
     override fun getItemCount(): Int {
-
+        return data.size + 1
     }
 
     override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
-
+        if (holder is DetailHeader) {
+            holder.description.text = description
+        } else if (holder is DetailLevel) {
+            holder.type.text = "${data[position - 1].first}: "
+            holder.level.text = data[position - 1].second.toString()
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
--- app/src/main/java/com/example/a27299/cats/home/fragment/species/SpeciesDetail.kt
+++ app/src/main/java/com/example/a27299/cats/home/fragment/species/SpeciesDetail.kt
@@ -1,8 +1,11 @@
 package com.example.a27299.cats.home.fragment.species
 
 import android.graphics.Color
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
 import com.bumptech.glide.Glide
 import com.example.a27299.cats.R
 import com.example.a27299.cats.module.Module
@@ -21,11 +24,33 @@ class SpeciesDetail : AppCompatActivity() {
             onBackPressed()
         }
         Module.speciesLiveData.value?.let {
+
             tv_species_detail_title.text = it[position].breeds[0].name
             Glide.with(this@SpeciesDetail)
                     .load(it[position].url)
                     .into(iv_species_detail_backdrop)
         }
+        rv_species_detail.adapter = DetailAdapter(parseData(), Module.speciesLiveData.value?.get(position)?.breeds?.get(0)?.description
+                ?: "无")
 
+        rv_species_detail.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun parseData(): ArrayList<Pair<String, Int>> {
+        val list = ArrayList<Pair<String, Int>>()
+        Module.speciesLiveData.value?.let {
+            it[position].breeds[0].apply {
+
+                list.add(Pair("affection_level", affection_level))
+                list.add(Pair("child_friendly", child_friendly))
+                list.add(Pair("dog_friendly", dog_friendly))
+                list.add(Pair("energy_level", energy_level))
+                list.add(Pair("health_issues", health_issues))
+                list.add(Pair("intelligence", intelligence))
+                list.add(Pair("social_needs", social_needs))
+
+            }
+        }
+        return list
     }
 }
